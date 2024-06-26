eb254c549d6c81ce1622736c0280540a7bb65f8f
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-06-26 14:44:14 +0300

    check

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,44 +1,87 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "./lib/hooks";
 
 export default function Home() {
+  const dispatch = useAppDispatch();
   const router = useRouter();
+  const { isLoggedIn, token, user } = useAppSelector((state) => ({
+    token: state.auth.token,
+    isLoggedIn: state.auth.isLoggedIn,
+    user: state.user.user
+  }));
   return (
     <>
-      <div>
-        <div className="relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-white overflow-hidden">
-          <div className="absolute inset-0">
-            <img
-              src="https://i.ibb.co/Wk6R9kj/tickin-high-resolution-logo.png"
-              alt="Background Image"
-              className="object-cover object-center w-full h-full"
-            />
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-          </div>
+      {token ? (
+        <div>
+          <div className="relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-white overflow-hidden">
+            <div className="absolute inset-0">
+              <img
+                src="https://i.ibb.co/Wk6R9kj/tickin-high-resolution-logo.png"
+                alt="Background Image"
+                className="object-cover object-center w-full h-full"
+              />
+              <div className="absolute inset-0 bg-black opacity-50"></div>
+            </div>
 
-          <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
-            <h1 className="text-5xl font-bold leading-tight mb-4">
-              Welcome to Our Awesome Website
-            </h1>
-            <p className="text-lg text-gray-300 mb-8">
-              Discover amazing features and services that await you.
-            </p>
-            <button
-              onClick={() => {
-                router.push("/login");
-              }}
-            >
-              <a
-                href="#"
-                className="bg-yellow-400 text-gray-900 hover:bg-yellow-300 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
+              <h1 className="text-5xl font-bold leading-tight mb-4">
+                Welcome to Our Awesome Website
+              </h1>
+              <p className="text-lg text-gray-300 mb-8">
+                Discover amazing features and services that await you.
+              </p>
+              <button
+                onClick={() => {
+                  router.push("/dashboard");
+                }}
               >
-                Get Started
-              </a>
-            </button>
+                <a
+                  href="#"
+                  className="bg-yellow-400 text-gray-900 hover:bg-yellow-300 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+                >
+                  Get Started
+                </a>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <div className="relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-white overflow-hidden">
+            <div className="absolute inset-0">
+              <img
+                src="https://i.ibb.co/Wk6R9kj/tickin-high-resolution-logo.png"
+                alt="Background Image"
+                className="object-cover object-center w-full h-full"
+              />
+              <div className="absolute inset-0 bg-black opacity-50"></div>
+            </div>
+
+            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
+              <h1 className="text-5xl font-bold leading-tight mb-4">
+                Welcome to Our Awesome Website
+              </h1>
+              <p className="text-lg text-gray-300 mb-8">
+                Discover amazing features and services that await you.
+              </p>
+              <button
+                onClick={() => {
+                  router.push("/login");
+                }}
+              >
+                <a
+                  href="#"
+                  className="bg-yellow-400 text-gray-900 hover:bg-yellow-300 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+                >
+                  Get Started
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
