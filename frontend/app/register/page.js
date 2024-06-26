27daf9eadd8365892cfa456b687181c3b9b90553
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-06-26 14:46:23 +0300

    check

--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -48,10 +48,10 @@ export default function Page() {
           >
             <img
               className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              src="https://i.ibb.co/MR2tFCg/Logo.png"
               alt="logo"
             />
-            Flowbite
+            
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
